6e39f5afac44725e3cb0d51a2683debd5fd2cb62
JinA <eszwaq951@gmail.com>
2022-05-10 01:13:21 +0800

    首頁更新
    首頁更新

---
+++ js/pagestyle.js
@@ -0,0 +1,56 @@
+
+
+    particlesJS(
+        'js-particles', {
+          'particles': {
+            'number': {
+              'value': 38
+            },
+            'color': {
+              'value': ['#466287', '#cecece', '#fff']
+            },
+            'shape': {
+              'type': 'circle'
+            },
+            'opacity': {
+              'value': 1,
+              'random': false,
+              'anim': {
+                'enable': false
+              }
+            },
+            'size': {
+              'value': 3,
+              'random': true,
+              'anim': {
+                'enable': false,
+              }
+            },
+            'line_linked': {
+              'enable': false
+            },
+            'move': {
+              'enable': true,
+              'speed': 2,
+              'direction': 'none',
+              'random': true,
+              'straight': false,
+              'out_mode': 'out'
+            }
+          },
+          'interactivity': {
+            'detect_on': 'canvas',
+            'events': {
+              'onhover': {
+                'enable': false
+              },
+              'onclick': {
+                'enable': false
+              },
+              'resize': true
+            }
+          },
+          'retina_detect': true
+      });
+
+
